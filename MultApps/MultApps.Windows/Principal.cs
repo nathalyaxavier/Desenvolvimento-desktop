0f03b4d65e9a374a2d503bd56223cd2a78b4998d
Nathalya Xavier <nathalyaxavier99@gmail.com>
2025-03-13 21:48:51 -0300

    feat:criamos uma tela com uma logo marca. #5

--- MultApps/MultApps.Windows/Principal.cs
+++ MultApps/MultApps.Windows/Principal.cs
@@ -23,5 +23,12 @@ namespace MultApps.Windows
             form.MdiParent = this;
             form.Show();
         }
+
+        private void Principal_Shown(object sender, EventArgs e)
+        {
+            var loading = new SplashScreen();
+            loading.ShowDialog();
+        }
     }
 }
+
---
+++ MultApps/MultApps.Windows/SplashScreen.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MultApps.Windows
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.pageSetupDialog2 = new System.Windows.Forms.PageSetupDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(181, 478);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(274, 18);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 518);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(236, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MultApps.Windows.Properties.Resources.Captura_de_tela_2025_03_13_201639;
+            this.ClientSize = new System.Drawing.Size(714, 559);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.progressBar1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            this.Shown += new System.EventHandler(this.SplashScreen_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
