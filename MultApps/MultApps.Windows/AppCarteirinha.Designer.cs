b6d1ea61c61ca13068dab15e2a121b004954826f
Nathalya Xavier <nathalyaxavier99@gmail.com>
2025-03-19 21:37:41 -0300

    feat: Desenvolvemos um Sistema de carteirinha. #9

---
+++ MultApps/MultApps.Windows/AppCarteirinha.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MultApps.Windows
+{
+    partial class AppCarteirinha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInformacao = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblDataNascimento = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtDataNascimento = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblIdadeCliente = new System.Windows.Forms.Label();
+            this.lblCPFCliente = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInformacao
+            // 
+            this.lblInformacao.AutoSize = true;
+            this.lblInformacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInformacao.Location = new System.Drawing.Point(171, 34);
+            this.lblInformacao.Name = "lblInformacao";
+            this.lblInformacao.Size = new System.Drawing.Size(451, 25);
+            this.lblInformacao.TabIndex = 0;
+            this.lblInformacao.Text = "Gerador de carteirinha acesso ao parque.";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(109, 111);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(44, 16);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.Location = new System.Drawing.Point(109, 202);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(33, 16);
+            this.lblCPF.TabIndex = 2;
+            this.lblCPF.Text = "CPF";
+            // 
+            // lblDataNascimento
+            // 
+            this.lblDataNascimento.AutoSize = true;
+            this.lblDataNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataNascimento.Location = new System.Drawing.Point(62, 277);
+            this.lblDataNascimento.Name = "lblDataNascimento";
+            this.lblDataNascimento.Size = new System.Drawing.Size(130, 16);
+            this.lblDataNascimento.TabIndex = 3;
+            this.lblDataNascimento.Text = "Data de Nascimento";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(40, 152);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(180, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(40, 234);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(180, 20);
+            this.txtCPF.TabIndex = 5;
+            // 
+            // txtDataNascimento
+            // 
+            this.txtDataNascimento.Location = new System.Drawing.Point(40, 326);
+            this.txtDataNascimento.Name = "txtDataNascimento";
+            this.txtDataNascimento.Size = new System.Drawing.Size(180, 20);
+            this.txtDataNascimento.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblIdadeCliente);
+            this.panel1.Controls.Add(this.lblCPFCliente);
+            this.panel1.Controls.Add(this.lblCliente);
+            this.panel1.Controls.Add(this.picBox2);
+            this.panel1.Location = new System.Drawing.Point(455, 111);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(227, 305);
+            this.panel1.TabIndex = 7;
+            // 
+            // lblIdadeCliente
+            // 
+            this.lblIdadeCliente.AutoSize = true;
+            this.lblIdadeCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdadeCliente.Location = new System.Drawing.Point(14, 244);
+            this.lblIdadeCliente.Name = "lblIdadeCliente";
+            this.lblIdadeCliente.Size = new System.Drawing.Size(130, 16);
+            this.lblIdadeCliente.TabIndex = 4;
+            this.lblIdadeCliente.Text = "Data de Nascimento";
+            // 
+            // lblCPFCliente
+            // 
+            this.lblCPFCliente.AutoSize = true;
+            this.lblCPFCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPFCliente.Location = new System.Drawing.Point(14, 200);
+            this.lblCPFCliente.Name = "lblCPFCliente";
+            this.lblCPFCliente.Size = new System.Drawing.Size(33, 16);
+            this.lblCPFCliente.TabIndex = 3;
+            this.lblCPFCliente.Text = "CPF";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(14, 149);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(44, 16);
+            this.lblCliente.TabIndex = 2;
+            this.lblCliente.Text = "Nome";
+            // 
+            // picBox2
+            // 
+            this.picBox2.Location = new System.Drawing.Point(26, 35);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(171, 108);
+            this.picBox2.TabIndex = 0;
+            this.picBox2.TabStop = false;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.Location = new System.Drawing.Point(22, 373);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(231, 31);
+            this.btnGerar.TabIndex = 8;
+            this.btnGerar.Text = "Gerar Carteirinha";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // AppCarteirinha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtDataNascimento);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblDataNascimento);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblInformacao);
+            this.Name = "AppCarteirinha";
+            this.Text = "AppCarteirinha";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInformacao;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblDataNascimento;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.TextBox txtDataNascimento;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.Label lblIdadeCliente;
+        private System.Windows.Forms.Label lblCPFCliente;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Button btnGerar;
+    }
+}
